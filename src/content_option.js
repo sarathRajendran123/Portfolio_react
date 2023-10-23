0f4f3f28fc7e923cfabcdfb348b47194d4abe28c
Sarath Rajendran <129683334+sarathRajendran123@users.noreply.github.com>
2023-10-23 20:29:05 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,128 @@
+const logotext = "ASWIN";
+const meta = {
+    title: "Sarath Rajendran",
+    description: "I'm Sarath Rajendran, Student at VIT Bhopal University",
+};
+
+const introdata = {
+    title: "I'm Sarath Rajendran",
+    animated: {
+        first: "Full Stack Developer",
+        second: "Application Developer",
+        third: "AIML Enthusiast",
+    },
+    description: "Currently a Third Year undergrad at VIT Bhopal, with a passion for creating pixel-perfect websites and cutting-edge algorithms",
+};
+
+const dataabout = {
+    title: "Who I am",
+    aboutme: "I'm Sarath Rajendran, a third-year undergraduate at VIT Bhopal University, majoring in Computer Science and Engineering. I come from Kozhikode also known as Calicut in Kerala. I'm a tech enthusiast with a penchant for MERN full stack development, and I've been known to Flutter around. I've got a crush on Machine Learning and AI, and Data Analysis is my secret indulgence"
+    };
+const worktimeline = [{
+        jobtitle: "Mern Full Stack Developer",
+        where: "Ethnus Training Centre",
+        date: "Aug '23 - Ongoing",
+    },
+    
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Java",
+        value: 70,
+    },
+    {
+        name: "Flutter",
+        value: 60,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 90,
+    },
+    {
+        name: "HTML",
+        value: 90,
+    },
+];
+
+const services = [{
+        title: "Will be updated soon",
+        description: ":))",
+    },
+    
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Project 1",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Project 2",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Project 3",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Project 4",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Project 5",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Project 6",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Project 7",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Project 8",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sarath.rajendran2021@gmail.com",
+};
+
+const socialprofils = {
+    github: "https://github.com/sarathRajendran123",
+    linkedin: "https://www.linkedin.com/in/sarath-rajendran-508287224/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
